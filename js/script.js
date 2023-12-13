74867a63177ef38557ab5b077f97be0d37b318e1
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-13 16:09:38 +0100

    config: struttura html js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+
+
+        }
+    },
+    methods: {
+
+    },
+    mounted(){
+        
+    }
+}).mount('#app');
